31ebfc4e191f784d0ab08f7c21f0e2bcdb3b5ffa
Jake Kraska <jake.kraska@gmail.com>
2019-01-15 20:07:04 +1100

    Add convert SEM function

---
+++ convertsem.R
@@ -0,0 +1,47 @@
+ConvertSEM <- function(sem, fromtype, totype) {
+  # Converts SEM scores based on the type of standardised score
+  #
+  # Args:
+  #   sem: a sem from a psychological test
+  #   fromtype: specify the type of sem: "z", "scaled", "standard" or "t"
+  #   toscore: specify the type of sem: "z", "scaled", "standard" or "t"
+  # Returns:
+  #   The sem in a converted standardised sem type
+  
+  # Check for missing arguments
+  if(missing(sem)) {stop("sem argument not provided.")}
+  if(missing(fromtype)) {stop("fromtype argument not provided.")}
+  if(missing(totype)) {stop("totype argument not provided.")}
+  
+  # Determine n
+  if (fromtype == "z") {
+    sd <- 1
+  } else if (fromtype == "scaled") {
+    sd <- 3
+  } else if (fromtype == "standard") {
+    sd <- 15
+  } else if (fromttype == "t") {
+    sd <- 10
+  } else {
+    stop("fromtype is specified incorrectly")
+  }
+  
+  n <- (sd/sem)^2
+  
+  # Determine new SEM
+  if (totype == "z") {
+    sd <- 1
+  } else if (totype == "scaled") {
+    sd <- 3
+  } else if (totype == "standard") {
+    sd <- 15
+  } else if (totype == "t") {
+    sd <- 10
+  } else {
+    stop("totype is specified incorrectly")
+  }
+  
+  sem <- sd/sqrt(n)
+  return(sem)
+
+}
